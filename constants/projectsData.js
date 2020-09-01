8ce9526a77168683f474c731491e5aa985b62775
Emmanuel <georgexx009@hotmail.com>
2020-09-01 15:30:09 -0500

    update projects data

--- constants/projectsData.js
+++ constants/projectsData.js
@@ -9,6 +9,26 @@ export const projectsData = [
   },
   {
     urlImg:
+      'https://res.cloudinary.com/dtwkxgaag/image/upload/v1593625746/projects/crud_application_with_user_validation_rre8ek.png',
+    title:
+      'Boilerplate with MERN Stack + Redux + JWT for user validation with CRUD operations',
+    summary:
+      'Boilerplate with MERN Stack + Redux + JWT for registration, login and authentication of users with CRUD operations for users posts.',
+    webPage: 'https://georgexx009.herokuapp.com',
+    github:
+      'https://github.com/georgexx009/user-auth-and-CRUD-app-MERN-boilerplate',
+  },
+  {
+    urlImg:
+      'https://res.cloudinary.com/dtwkxgaag/image/upload/v1593625745/projects/default_project_pfygxu.png',
+    title:
+      'Blog app (Boilerplate) with SSR and Markdown files consume (Working on it)',
+    summary:
+      'This is a Boilerplate app for a blog webpage. It uses Server Side Rendering with Webpack 4 and consume Markdown files for populate post pages. ',
+    github: 'https://github.com/georgexx009/blog-app-ssr-markdown',
+  },
+  {
+    urlImg:
       'https://res.cloudinary.com/dtwkxgaag/image/upload/v1595444104/projects/portfolio-v2_bi7tnn.png',
     title: 'Portfolio on Next.js',
     summary:
@@ -18,52 +38,10 @@ export const projectsData = [
   },
   {
     urlImg:
-      'https://res.cloudinary.com/dtwkxgaag/image/upload/v1595449048/projects/habit-tracker_zzuo3f.png',
-    title: 'Habit Tracker - Coming soon',
-    summary:
-      'A progresive web application that will focus on help you build good habits, stop bad habits and stop procrastination. ',
-    github: 'https://github.com/georgexx009/habit-tracker',
-  },
-  {
-    urlImg:
-      'https://res.cloudinary.com/dtwkxgaag/image/upload/v1593625746/projects/crud_application_with_user_validation_rre8ek.png',
-    title: 'CRUD Application with user validation',
-    summary:
-      'It is an application where a user could log in, register, and make CRUD operations for their own posts only. This application uses JWT for validating the user.',
-    github: 'https://github.com/georgexx009/my-playground/tree/master/my-store',
-    webPage: 'https://georgexx009.herokuapp.com/myPlayground/#/MyPublications',
-  },
-  {
-    urlImg:
-      'https://res.cloudinary.com/dtwkxgaag/image/upload/v1593625748/projects/portfolio_fzsfcf.png',
-    title: 'Portfolio on React and Webpack',
-    summary:
-      'This is the first version of my portfolio. A Dark mode Single Page Website. Read about me and what I have done.',
-    github: 'https://github.com/georgexx009/emmanuel-portfolio',
-    webPage: 'https://georgexx009.herokuapp.com',
-  },
-  {
-    urlImg:
-      'https://res.cloudinary.com/dtwkxgaag/image/upload/v1593625749/projects/simple_crud_application_bcdyy5.png',
-    title: 'Simple CRUD Application',
-    summary: 'It is simple CRUD application.',
-    github: 'https://github.com/georgexx009/my-playground/tree/master/my-store',
-    webPage: 'https://georgexx009.herokuapp.com/myPlayground/#/cards',
-  },
-  {
-    urlImg:
       'https://res.cloudinary.com/dtwkxgaag/image/upload/v1593625745/projects/default_project_pfygxu.png',
     title: 'Data Structures and Algorithms in Python',
     summary:
       'These are the codes that I do while learning Data Structures and Algorithms',
     github: 'https://github.com/georgexx009/Data-Structures-python',
-  },
-  {
-    urlImg:
-      'https://res.cloudinary.com/dtwkxgaag/image/upload/v1593625745/projects/default_project_pfygxu.png',
-    title: 'Rest API - Node.js web server',
-    summary:
-      'This web server provide a REST API for my applications. The data is save on a MongoDB.',
-    github: 'https://github.com/georgexx009/rest-server',
   },
 ];
